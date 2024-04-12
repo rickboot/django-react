ad7696425ddeede8bb63c9801840182d19bec136
Rick Allen <rickallen@gmail.com>
2024-04-11 22:46:09 -0700

    fix register redirect and add choreo url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,16 +1,21 @@
 import axios from 'axios';
 import { ACCESS_TOKEN } from './constants';
 
+const viteUrl = import.meta.env.VITE_API_URL;
+const choreoUrl = '/choreo-apis/django-react/backend/rest-api-be2/v1.0';
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: viteUrl ? viteUrl : choreoUrl,
 });
 
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem(ACCESS_TOKEN);
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
   },
   (error) => {
